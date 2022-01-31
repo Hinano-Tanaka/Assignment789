0a5bf7a1bded86d8361d06d81ec2f3baad2c687d
Hinano-Tanaka <95991554+Hinano-Tanaka@users.noreply.github.com>
2022-01-31 04:29:25 +0000

    [Update] HasManyThroughOrderError fixed

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,19 +2,19 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :tags, through: :book_tags
   has_many :book_tags, dependent: :destroy
-  
+  has_many :tags, through: :book_tags
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
-  
-  
-  
+
+
+
+
   def save_tags(savebook_tags)
     # 現在のユーザーの持っているskillを引っ張ってきている
     current_tags = self.tags.pluck(:name) unless self.tags.nil?
@@ -22,12 +22,12 @@ class Book < ApplicationRecord
     old_tags = current_tags - savebook_tags
     # 今回保存されたものと現在の差を新しいタグとする。新しいタグは保存
     new_tags = savebook_tags - current_tags
-		
+
     # Destroy old taggings:
     old_tags.each do |old_name|
       self.tags.delete Tag.find_by(name:old_name)
     end
-		
+
     # Create new taggings:
     new_tags.each do |new_name|
       book_tag = Tag.find_or_create_by(name:new_name)
@@ -35,9 +35,9 @@ class Book < ApplicationRecord
       self.tags << book_tag
     end
   end
-  
-  
-  
+
+
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
@@ -49,5 +49,5 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%'+content+'%')
     end
   end
-  
+
 end
