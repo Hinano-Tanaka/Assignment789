a04dc07d645113cc23b4f204bbbb831fd0317f3a
Hinano-Tanaka <95991554+Hinano-Tanaka@users.noreply.github.com>
2022-01-31 04:23:53 +0000

    [Finish] 課題９

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,17 +11,21 @@ class BooksController < ApplicationController
     @books = Book.all.order(params[:sort])
     @book = Book.new
   end
-
+  
+  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    tag_list = params[:book][:tag_name].split(',')
     if @book.save
+      @book.save_tags(tag_list)
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
     end
   end
+  
 
   def edit
   end
